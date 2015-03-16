6fb7b11a156f4204620ade2933bd6129b08e360c
Alex <graale@list.ru>
2015-03-17 02:45:04 +0300

    fix strict mode issue

--- src/plugins/objectof.js
+++ src/plugins/objectof.js
@@ -25,14 +25,16 @@ function isGlobal(g) {
 
 
 pluginObjectOf.ObjectOf = function objectOf(globOrFunc, funcOrProps, args) {
+
+  function createMatcher(props) {
+    return new ObjectMatcher(globOrFunc, props);
+  }
+
   if (isGlobal(globOrFunc)) {
     //constructor called as function, matching case
     return new ObjectMatcher(funcOrProps, args[0]);
   }
   if (typeof globOrFunc === 'function') {
-    function createMatcher(props) {
-      return new ObjectMatcher(globOrFunc, props);
-    }
     return funcOrProps === undefined ? createMatcher : createMatcher(funcOrProps);
   }
   //this is normally created object, ignore
